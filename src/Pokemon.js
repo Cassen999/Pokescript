c5634d3fb67e10aad4886dbbd406e16255eb25c4
Cassen Gerber <cassenpt@gmail.com>
2021-02-22 17:23:09 -0600

    finished all setup from video 1

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Pokemon = () => {
+const Pokemon = (props) => {
+    const { match } = props;
+    const { params } = match;
+    const { pokemonId } = params;
     return(
-        <div>This is the pokemon page</div>
+        <div>{`This is the pokemon page for pokemon #${pokemonId}`}</div>
     )
 }
 
