10678e1dba3cedd2c531a07ae0b0aa0f358a97c1
Cassen Gerber <cassenpt@gmail.com>
2021-02-23 14:35:40 -0600

    configured some settings

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import mockData from './MockData';
 
 const useStyles = makeStyles({
     pokedexContainer: {
@@ -23,18 +24,22 @@ const getPokemonCard = () => {
 
 const Pokedex = () => {
     const classes = useStyles();
+    // pokemonData is the getter
+    // setPokemonData is the setter
+    // useState(mockData) sets the mockData as the default state
+    const [pokemonData, setPokemonData] = useState(mockData);
     return(
         <>
-        <AppBar position="static">
-            <Toolbar />
-        </AppBar>
-        <Grid container spacing={2} className={classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-        </Grid>
+            <AppBar position="static">
+                <Toolbar />
+            </AppBar>
+            <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+                    {getPokemonCard()}
+            </Grid>
         </>
     )
 }
