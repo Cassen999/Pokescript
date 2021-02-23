69420c8f25f8e8451961133496200b7160d61206
Cassen Gerber <cassenpt@gmail.com>
2021-02-23 17:19:04 -0600

    working on getting pokemon info into a card

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,9 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { Typography, Link, CircularProgress, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { toFirstCharUppercase } from './constants';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import clsx from 'clsx';
 import axios from 'axios';
 
+const useStyles = makeStyles(theme => ({
+  infoContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+    margin: '5px',
+  },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 'fit',
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
 const Pokemon = (props) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   const { history, match } = props;
   const { params } = match;
   const { pokemonId } = params;
@@ -31,6 +80,68 @@ const Pokemon = (props) => {
     const { front_default } = sprites;
     return(
       <>
+      <center>
+      <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image={fullImageUrl}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          This impressive 
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 .
+          </Typography>
+          <Typography paragraph>
+            Heat oil
+          </Typography>
+          <Typography paragraph>
+            Add rice
+          </Typography>
+          <Typography>
+            Set aside
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
         <Typography variant='h1'>
           {`${id}.`} {toFirstCharUppercase(name)}
           <img src={front_default} />
@@ -49,20 +160,23 @@ const Pokemon = (props) => {
           const { name } = type;
           return <Typography key={name}>{`${name}`}</Typography>;
         })}
+      </center>
       </>
     );
   }
   return(
-   <>
+   <div className={classes.infoContainer}>
     {pokemon === undefined && <CircularProgress />}
     {pokemon !== undefined && pokemon && generatePokemonJsx()}
     {pokemon === false && <Typography>Pokemon not found</Typography>}
     {pokemon !== undefined && (
-      <Button variant='contained' onClick={() => history.push('/')}>
-        Back to Pokedex
-      </Button>
+      <center>
+        <Button variant='contained' onClick={() => history.push('/')}>
+          Back to Pokedex
+        </Button>
+      </center>
     )}
-   </>
+   </div>
   );
 }
 
