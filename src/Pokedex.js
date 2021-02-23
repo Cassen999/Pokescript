aa7f11e279d5a223fdf7edb5b274e8f2f13e76d8
Cassen Gerber <cassenpt@gmail.com>
2021-02-23 15:18:50 -0600

    putting in test data

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,47 +1,68 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Grid, Card, CardContent } from '@material-ui/core';
+import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import mockData from './MockData';
 
 const useStyles = makeStyles({
-    pokedexContainer: {
-        paddingTop: '20px',
-        paddingLeft: '50px',
-        paddingRight: '50px',
-    }
+  pokedexContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+  },
+  cardMedia: {
+    margin: 'auto'
+  }
 })
 
-const getPokemonCard = () => {
-    return(
-        <Grid item xs={12} sm={4}>
-            <Card>
-                {/* using cardcontent helps with spacing */}
-                <CardContent>hi</CardContent>
-            </Card>
-        </Grid>
-    )
-}
 
-const Pokedex = () => {
-    const classes = useStyles();
-    // pokemonData is the getter
-    // setPokemonData is the setter
-    // useState(mockData) sets the mockData as the default state
-    const [pokemonData, setPokemonData] = useState(mockData);
+const Pokedex = (pokemonId) => {
+  const classes = useStyles();
+
+  // pokemonData is the getter
+  // setPokemonData is the setter
+  // useState(mockData) sets the mockData as the default state
+  const [pokemonData, setPokemonData] = useState(mockData);
+
+  const toFirstCharUppercase = name => 
+  // Makes first character uppercase and .slice is appending the rest of the name
+    name.charAt(0).toUppercase() + name.slice(1)
+  
+  
+  const getPokemonCard = (pokemonId) => {
+    const { id, name } = pokemonData[`${pokemonId}`];
+    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    console.log(pokemonData[`${pokemonId}`])
     return(
-        <>
-            <AppBar position="static">
-                <Toolbar />
-            </AppBar>
-            <Grid container spacing={2} className={classes.pokedexContainer}>
-                    {getPokemonCard()}
-                    {getPokemonCard()}
-                    {getPokemonCard()}
-                    {getPokemonCard()}
-                    {getPokemonCard()}
-            </Grid>
-        </>
+      <Grid item xs={12} sm={4} ley={pokemonId}>
+        <Card>
+          <CardMedia
+            className={classes.cardMedia}
+            image={sprite}
+            style={{width: '130px', height: '130px'}} />
+          {/* using cardcontent helps with spacing */}
+          <CardContent>
+            <Typography>{`${id}, ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+    </Grid>
     )
+  }
+  return(
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      {pokemonData ? (
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map((pokemonId) => 
+            getPokemonCard(pokemonId)
+          )}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  )
 }
 
 export default Pokedex
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Pokemon = (props) => {
-    const { match } = props;
-    const { params } = match;
-    const { pokemonId } = params;
-    return(
-        <div>{`This is the pokemon page for pokemon #${pokemonId}`}</div>
-    )
+  const { match } = props;
+  const { params } = match;
+  const { pokemonId } = params;
+  return(
+    <div>{`This is the pokemon page for pokemon #${pokemonId}`}</div>
+  )
 }
 
 export default Pokemon
