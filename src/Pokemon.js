e18742709a6f30ffcb971feac1d595a7c2966d5d
Cassen Gerber <cassenpt@gmail.com>
2021-02-24 13:58:48 -0600

    removed card from pokemon.js, deciding on a different format for render

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -81,67 +81,6 @@ const Pokemon = (props) => {
     return(
       <>
       <center>
-      <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        className={classes.media}
-        image={fullImageUrl}
-        title="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive 
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 .
-          </Typography>
-          <Typography paragraph>
-            Heat oil
-          </Typography>
-          <Typography paragraph>
-            Add rice
-          </Typography>
-          <Typography>
-            Set aside
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
         <Typography variant='h1'>
           {`${id}.`} {toFirstCharUppercase(name)}
           <img src={front_default} />
